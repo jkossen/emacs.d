afa772188eefb636300f738731dce93927bf00e2
Jochem Kossen <jochem@jkossen.nl>
2022-06-11 10:52:20 +0200

    enable automatic push for git-auto-commit-mode

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
  '(exec-path
    '("/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_14" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin" "/Users/jochem/go/bin" "/usr/local/bin" "/opt/bin"))
  '(fringe-mode '(0 . 5) nil (fringe))
+ '(gac-automatically-push-p t)
  '(indent-tabs-mode nil)
  '(inhibit-startup-screen t)
  '(ivy-display-style 'fancy)
@@ -61,7 +62,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-todo ((t (:inherit hl-todo :italic t)))))
 
 (setq
  default-directory "~/"
